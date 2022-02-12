01425b5884d0e591ab56d485715d4340b0aac574
Copotter <allman956@naver.com>
2022-02-12 16:41:36 +0900

    Init: Emotion

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,23 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import styled from '@emotion/styled';
+import Link from 'next/link';
+
+import Layout from '../components/Layout';
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
+    <EmotionDiv>
+      <h1>Hello Next.js 👋</h1>
+      <p>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </p>
+    </EmotionDiv>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
+
+const EmotionDiv = styled.div`
+  background-color: yellow;
+`;
